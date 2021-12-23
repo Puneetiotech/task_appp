70f3a7884d7de122ac6582af79e2e125a8086734
Puneet kukreja <puneetk@iotech.biz>
2021-12-23 16:19:57 +0530

    fifth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:appcenter/appcenter.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:appcenter_analytics/appcenter_analytics.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:appcenter_crashes/appcenter_crashes.dart';
 
 void initAppCenter() async {
