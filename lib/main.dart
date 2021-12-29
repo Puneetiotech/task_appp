bd9d8469bc2969d8e1f3aba055818412c17d5642
Puneet kukreja <puneetk@iotech.biz>
2021-12-29 17:25:05 +0530

    new build for dev

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home CICD for dev'),
+      home: const MyHomePage(title: 'Flutter Demo Home CICD for dev new build'),
     );
   }
 }
