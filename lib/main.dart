6a1e8c69394c1e98f8d9bfe614e3108f7a92391a
Puneet kukreja <puneetk@iotech.biz>
2021-12-30 12:41:41 +0530

    added secret key again for dev

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    //initAppCenter();
+    initAppCenter();
     initalSetup();
   }
 
